55f89a141d0437e495613bc61f225426057df844
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-05-05 01:24:04 +0600

    Day - 0 of HackerRank: 10 days of Javascript complete

--- HackerRank-10-days-of-Javascript/Day-0/Hello-World/index.js
+++ HackerRank-10-days-of-Javascript/Day-0/Hello-World/index.js
@@ -63,29 +63,9 @@ function readLine() {
     return inputString[currentLine++];
 }
 
-'use strict';
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
 
-let inputString = '';
-let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.trim().split('\n').map(string => {
-        return string.trim();
-    });
-    
-    main();    
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
 
 /**
 *   A line of code that prints "Hello, World!" on a new line is provided in the editor. 
@@ -97,18 +77,13 @@ function readLine() {
 function greeting(parameterVariable) {
     // This line prints 'Hello, World!' to the console:
     console.log('Hello, World!');
-    console.log(parameterVariable);
 
     // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
     
 }
 
 
-function main() {
-    const parameterVariable = readLine();
-    
-    greeting(parameterVariable);
-}
 
 
 function main() {
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # practice-problem
+
+<!-- HackerRank -->
+
+## 10 days of Javascript
+
+1. Day 0: Hello, World!, Data Types.
+

cfe560b6014e0fb81b3c392cc06ea0d68dd291d8
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-06-12 06:12:59 +0600

    module - 21 and 21.5 practice problem complete using function

--- Programming-hero-coding-chalenge/module-21.5:JS-Concept-Recap/index.js
+++ Programming-hero-coding-chalenge/module-21.5:JS-Concept-Recap/index.js
@@ -1,7 +1,9 @@
 /*
 ১. আমি যদি বলি তিনটা ভেরিয়েবল ডিক্লেয়ার করতে হবে। সেটা তোমাকে পারতে হবে। 
 */
-
+let numbers = [3, 4, 5];
+let myName = 'Nusrat';
+const isStudent = true;
 
 
 
@@ -10,7 +12,9 @@
 ২. একটা লুপ লিখতে হবে যেটা ১ থেকে ১০০ পর্যন্ত যত সংখ্যা আছে সেটা দেখাবে 
 */
 
-
+for (let i = 1; i <= 100; i++){
+   console.log(i);
+}
 
 
 
@@ -18,25 +22,30 @@
 ৩. ৫০ থেকে ৮০ এর মধ্যে যতো বিজোড় সংখ্যা আছে সেগুলাকে দেখাবে। 
 */
 
-
+for (let i = 51; i <= 80; i = i + 2){
+   console.log(i);
+}
 
 
 
 /*৪. তিনটা সংখ্যা এর যোগ করতে পারবে এমন একটা ফাংশন লিখো 
 */
 
-
-
-
+function addThreeNumbers(num1, num2, num3) {
+    let sum = num1 + num2 + num3;
+    return sum;
+}
+console.log(addThreeNumbers(3, 4, 5));
 
 
 /*
 5. তোমাকে ফাংশনের ইনপুট হিসেবে সেলসিয়াস দিবে। তুমি ক্যালকুলেশন করে তাপমাত্রা ফারেনহাইট এ কনভার্ট করে সেটার আউটপুট রিটার্ন করবে 
 */
-
-
-
-
+function celciusToFahrenheit(celcius) {
+  let fahrenheit = celcius * 9 / 5 + 32;
+  return fahrenheit;
+}
+console.log(celciusToFahrenheit(30));
 
 
 
@@ -44,7 +53,11 @@
 6. একইভাবে উল্টা হিসাব করবে। যাতে তোমাকে ফারেনহাইট হিসেবে তাপমাত্রা দিলে সেটাকে সেলসিয়াস এ কনভার্ট করে আউটপুট দিবে। 
 */
 
-
+function fahrenheitToCelsius(fahrenheit) {
+  let celsius = (5 / 9) * (fahrenheit - 32);
+  return celsius;
+}
+console.log(fahrenheitToCelsius(122));
 
 
 
@@ -55,12 +68,80 @@
 */
 
 
-
-
-
-
+function getGrades(marks) {
+    if (marks >= 90) {
+        return 'A+';
+    }
+    else if(marks >= 80){
+        return 'A';
+    }
+    else if(marks >= 70){
+        return 'A-';
+    }
+    else if(marks >= 60){
+        return 'B';
+    }
+     else if(marks >= 50){
+        return 'C';
+    }
+    else {
+        return 'F';
+    }
+}
+const marks = 80;
+const grade = getGrades(marks);
+console.log(grade);
 
 
 /*
 9. সুদের হিসাব করবে। জাস্ট হিসাব কেমনে করতে হয়। সেই চিন্তায় করবে। সুদ ভালো না খারাপ সেটা এখন চিন্তা করার দরকার নাই। জাস্ট একটা ফর্মুলা থাকলে সেটা কিভাবে কোড এ লিখতে হয় সেই এঙ্গেল থেকে করার চেষ্টা করো। 
+*/
+
+/*
+//explanation:
+In this code, we define the calculateInterest() function that takes three parameters: principal, rate, and time.
+
+The function multiplies the principal amount by the rate and time to calculate the interest, which is stored in the interest variable.
+
+We then define the values for principal, rate, and time. In this example, we set principal to 1000, rate to 0.05 (which represents a 5% interest rate), and time to 2 years.
+
+Next, we call the calculateInterest() function with the provided values and assign the result to the interest variable.
+
+Finally, we log the calculated interest to the console using console.log().
+
+This means that for a principal amount of 1000, an interest rate of 5%, and a time period of 2 years, the calculated interest is 100.
+*/
+
+function calculateInterest(principle, rate, time) {
+    const interest = principle * rate * time;
+    return interest;
+}
+
+const principle = 1000;
+const rate = 0.05;
+const time = 2; // 2 years
+
+const interest = calculateInterest(principle, rate, time);
+console.log("Interest", interest);
+
+
+
+/*
+একটু কোড লিখে অবজেক্ট এর সব প্রপার্টি এর নাম এবং মান আউটপুট হিসেবে দেখাও। 
+*/
+const person = {
+  name: "John Doe",
+  age: 30,
+  occupation: "Engineer",
+  city: "New York"
+};
+
+Object.entries(person).forEach(([key, value]) => {
+  console.log(`${key}: ${value}`);
+});
+
+/*
+In this code, we use the Object.entries() method to convert the object's properties into an array of key-value pairs. Each key-value pair is represented as an array [key, value].
+
+We then use the forEach() method to iterate over each key-value pair. Within the loop, we destructure the [key, value] array and log the key and value using console.log().
 */
--- Programming-hero-coding-chalenge/module-21:apply-javascript-concept/index.js
+++ Programming-hero-coding-chalenge/module-21:apply-javascript-concept/index.js
@@ -1,24 +1,94 @@
 //প্রাকটিস চ্যালেঞ্জ-৩ টা: 
+
 /*
+12. write a function that will take hour as the input parameter and will convert it into minutes and will return the result in minutes. / convert minute to second.
+৩. এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে। 
+*/
+
+function hourToMinutes(hour) {
+  let minutes = hour * 60;
+  return minutes;
+
+}
+const hours = 5;
+const minutes = hourToMinutes(hours);
+console.log(`${hours} is equal to ${minutes} minutes`);
+
+
+/* 13. write a function findLeapYear() that will take the array [2023, 2024, 2025, 2028, 2030] as the input parameter and will check if each year is a leap year. If a year is a leap year 
+insert that year in a new array, return the new array and print the result. (odd number ber system er moto)
 ১. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
 */
 
 
-
-
-
 /*
+// explanation
+
+In this code, the findLeapYear() function takes an array of years as input. It initializes an empty array called leapYears to store the leap years.
+
+The function then iterates over each year in the input array using a for loop. For each year, it checks if the year is divisible by 4 but not divisible by 100 (except if it's divisible by 400), which are the conditions for a leap year. If a year satisfies these conditions, it is added to the leapYears array using the push() method.
+
+Finally, the function returns the leapYears array.
+
+In the example usage, we pass the array [2023, 2024, 2025, 2028, 2030] to the findLeapYear() function. The function identifies that the years 2024 and 2028 are leap years and returns a new array [2024, 2028]. We log this array to the console using console.log().
+
+*/
+
+function findLeapYear(years) {
+  const leapYears = [];
+
+  for (let i = 0; i < years.length; i++){
+    const year = years[i];
+
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      leapYears.push(year);
+    }
+  }
+  return leapYears;
+}
+let years = [2023, 2024, 2025, 2028, 2030];
+let leapYears = findLeapYear(years);
+console.log('leap years:', leapYears);
+
+
+
+
+/* 14. Write a function findOddSum() that will take the array [5, 7, 8, 45, 30] as the input parameter and will return the sum of the odd numbers.
 ২. তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে।
 */
 
 
-
-
-
-
 /*
-৩. এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে। 
+
+// explanation:
+In this code, the findOddSum() function takes an array of numbers as input. It initializes a variable sum to 0 to keep track of the sum of odd numbers.
+
+The function then iterates over each number in the input array using a for loop. For each number, it checks if the number is odd by checking if the remainder of dividing the number by 2 is not equal to 0. If the number is odd, it adds it to the sum variable using the += operator.
+
+Finally, the function returns the sum variable, which represents the sum of the odd numbers in the array.
+
+In the example usage, we pass the array [5, 7, 8, 45, 30] to the findOddSum() function. The function calculates the sum of the odd numbers (5, 7, and 45) in the array, which results in 57. We log this sum to the console using console.log().
 */
+
+function findOddSum(numbers) {
+  let sum = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    const number = numbers[i];
+
+    if (number % 2 !== 0) {
+      sum += number;
+    }
+  }
+
+  return sum;
+}
+
+// Example usage
+const numbers = [5, 7, 8, 45, 30];
+const oddSum = findOddSum(numbers);
+console.log("Sum of odd numbers:", oddSum);
+
 
 
 /*
@@ -111,8 +181,8 @@ isEven(303);
 ***Kono year leap year hobe kina oita bujhbo jodi year ta 4 diye vag jay.
 */
 function isLeapYear(year){
-   const remainder = year % 4;
-   if(remainder === 0){
+   const leapYear = year % 4;
+   if(leapYear === 0){
       //console.log('Your year is leap year', year);
       return true;
     }
@@ -128,6 +198,8 @@ console.log('Her year:', isHerYearLeapYear);
 
 // another way
 
+//  jodi wkhane ans true hoy tahole r porer line gulote jabena or jodi false hoy tahole (if-else) condition evebeo likha jay
+
 function isLeapYear(year){
     const remainder = year % 4;
     if(remainder === 0){
@@ -136,12 +208,12 @@ function isLeapYear(year){
      }
     return false;
 }
-//  jodi wkhane ans true hoy tahole r porer line gulote jabena or jodi false hoy tahole (if-else) condition evebeo likha jay
-// const isMyYearLeapYear = isLeapYear(1933);
-// console.log('my year:', isMyYearLeapYear);
 
-// const isHerYearLeapYear = isLeapYear(1960);
-// console.log('Her year:', isHerYearLeapYear);
+const isMyYearLeapYear = isLeapYear(1933);
+console.log('my year:', isMyYearLeapYear);
+
+const isHerYearLeapYear = isLeapYear(1960);
+console.log('Her year:', isHerYearLeapYear);
 
 
 ////////////////////////////////////////////////////////
@@ -150,7 +222,9 @@ function isLeapYear(year){
 8. Write  a function findOddSum() that will take the array [5,7,8,10,45,30] as the input prameter and will return the sum of the odd numbers.
 */
 
-function sumOfAnArray(numbers){
+
+// sum of all numbers
+function findOfSum(numbers){
     // 2.(i) condition er bahire sum er kaj korbo (jokhon konokichu add korbo tokhon er primary value hobe 0 cause ekhono add korinai)
     let sum = 0; // sum jodi for ondition er vitore declare kori then each time new line e add korte hobe 0 er shathe ager addition er value hariye jabe jodi kothao store kore na rakhi that' why sum for loop er bahire declare korbo.
   //console.log(numbers);
@@ -168,12 +242,12 @@ function sumOfAnArray(numbers){
       return sum;
 
 }
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-sumOfAnArray(myNumbers);
+const myNumbers = [5, 7, 8, 10, 45, 30];
+findOfSum(myNumbers);
 
 
 // without comments
-function sumOfAnArray(numbers){
+function findOfSum(numbers){
     let sum = 0;
     for(let i = 0; i < numbers.length; i++){
         const index = i;
@@ -183,11 +257,38 @@ function sumOfAnArray(numbers){
   }
   return sum;
 }
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-sumOfAnArray(myNumbers);
+const myNumbers = [5, 7, 8, 10, 45, 30];
+console.log(findOfSum(myNumbers));
 
 
 //////////////////////////////
+
+// find the sum of odd numbers
+
+/*
+
+In this function, we initialize the variable sum to 0. Then, we iterate over each element of the array using a for loop. Inside the loop, we use the modulo operator (%) to check if the current element is odd (array[i] % 2 !== 0). If it is, we add it to the sum variable. Finally, we return the sum after looping through all the elements in the array.
+
+In the example usage, we create an array [5, 7, 8, 10, 45, 30], and then call the findOddSum() function with this array as the parameter. The resulting sum of the odd numbers is stored in the oddSum variable, which we then log to the console.
+
+
+*/
+function findOddSum(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 !== 0) {
+      sum += array[i];
+    }
+  }
+  return sum;
+}
+
+// Example usage:
+const array = [5, 7, 8, 10, 45, 30];
+const oddSum = findOddSum(array);
+console.log(oddSum); // Output: 57 (5 + 7 + 45)
+
+
 
 /*
 9. kono 1 ta array te jeshokol bijor(odd) number ase shegulo ber korbo and add korbo
@@ -216,9 +317,29 @@ const myNumbers = [12, 65, 45, 78, 32, 45, 91];
 const oddNumbers = getOddNumbersOfAnArray(myNumbers);
 console.log(oddNumbers);
 // 6. 8 no. problem theke sum value anbo (eta alada vabe pore kore dekhte hobe)
-const oddNumberSum = sumOfAnArray(oddNumbers);
+const oddNumberSum = findOddSum(oddNumbers);
 console.log('odd number sum', oddNumberSum);
 
+//////////// full code //////////////
+
+function getOddNumbersOfAnArray(numbers) {
+  const oddNumbers = [];
+
+  for (let i = 0; i < numbers.length; i++){
+    const index = i;
+    const element = numbers[index];
+    if(element % 2 !== 0){
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+    }
+    return oddNumbers;
+}
+const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumberSum = sumOfAnArray(oddNumbers);
+console.log('odd number sum', oddNumberSum);
 
 
 //////////////////////////////////////////
@@ -240,13 +361,13 @@ function sumOfNumbers(number){
     }
     return sum;
 }
-
+sumOfNumbers(7);
 // multiplication of numbers
 
 function multiplicationOfNumbers(number){
     // multiplication er shomoy initial value always 1 rakhbo
    let result = 1;
-   for(i = 1; i <= number; i++){
+   for(let i = 1; i <= number; i++){
       result = result * i;
     }
     return result;
@@ -261,31 +382,97 @@ console.log(result);
 // factorial formula: n! = n * (n - 1)!.
 
 function factorial(number){
-   let result = 1;
+   let factorial = 1;
    for(let i = number; i >= 1; i--){
-      result = result * i;
-      console.log(i);
+      factorial = factorial * i;
+      console.log(i, factorial);
    }
-   return result;
+   return factorial;
 }
 const number = 5;
 const fact = factorial(number);
 console.log('factorial of:',number, fact);
 
+//////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////////
+//////   using recursion//////////////////
+
+/*Certainly! Here's a concise way to calculate the factorial of a number using recursion in JavaScript: */
+
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+  
+  return number * factorial(number - 1);
+}
+
+// Example usage
+const number = 5;
+const factorialOfNumber = factorial(number);
+console.log(`The factorial of ${number} is ${factorialOfNumber}`);
+
+/*
+In this code, the factorial function takes a number as input.It checks for the base cases where the number is 0 or 1, and in both cases, it returns 1. This is because the factorial of 0 or 1 is always 1.
+
+If the input number is greater than 1, the function recursively calls itself with the argument number - 1 and multiplies it by the current number. The recursion continues until it reaches the base case, at which point the function starts unwinding and returning the factorial value back up the call stack.
+
+In the example usage, we calculate the factorial of the number 5 and log the result to the console. You can replace the value of number with any positive integer to calculate its factorial using this function.
+
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 // 11. Calculate Factorial of a number using while loop:
 
-function factorial(number){
-   let i = 1;
-   let result = 1;
-   while(i <= 7){
-        result = result * i;
-        i++;
-   }
-   return result;
+function factorial(number) {
+  let factorial = 1;
+  let currentNumber = number;
+  while (currentNumber > 1) {
+    factorial = factorial * currentNumber;
+    currentNumber--;
+    console.log(factorial);
+  }
+  return factorial;
 }
+let number = 5;
+let factorialOfNumber = factorial(number);
+console.log(`the factorial of ${number} is ${factorialOfNumber}`);
+
+
+// //////////////////////////////////////////////////////////////////////////////////////////////factorial while solution in chatgpt
+
+
+function factorial(number) {
+  if (number === 0 || number === 1) {
+    return 1;
+  }
+
+  let result = 1;
+  let currentNumber = number;
+
+  while (currentNumber > 1) {
+    result *= currentNumber;
+    currentNumber--;
+  }
+
+  return result;
+}
+// Example usage
+const number = 5;
+const factorialOfNumber = factorial(number);
+console.log(`The factorial of ${number} is ${factorialOfNumber}`);
+/*
+In this code, we define a function called factorial that takes a number as input. Inside the function, we first handle the base cases where the number is 0 or 1, in which case the factorial is always 1.
+
+If the input number is greater than 1, we initialize result to 1 and currentNumber to the input number. Then we enter a while loop that multiplies result by currentNumber and decrements currentNumber by 1 in each iteration until currentNumber is no longer greater than 1.
+
+Finally, we return the result, which will be the factorial of the input number.
+
+In the example usage, we calculate the factorial of the number 5 and log the result to the console. You can replace the value of number with any positive integer to calculate its factorial using this function.
+*/
+
 
 // factorial while in a reverse way
 function factorial(number){
@@ -303,25 +490,7 @@ console.log(output);
 ///////////////////////////////
 
 /*
-12. write a function that will take hour as the input parameter and will convert it into minutes and will return the result in minutes. / convert minute to second.
-*/
 
 
 
 
-
-
-
-
-
-
-/* 13. write a function findLeapYear() that will take the array [2023, 2024, 2025, 2028, 2030] as the input parameter and will check if each year is a leap year. If a year is a leap year 
-insert that year in a new array, return the new array and print the result. (odd number ber system er moto)
-*/
-
-
-
-
-
-/* 14. Write a function findOddSum() that will take the array [5, 7, 8, 45, 30] as the input parameter and will return the sum of the odd numbers.
-*/

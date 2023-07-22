061b94eab9d83d47a206932700b2aa496c986793
Nusrat Shauoly <61565311+nusratshauoly@users.noreply.github.com>
2023-07-23 05:09:49 +0600

    folder- changes

--- practice/index.js
+++ practice/index.js
@@ -1,23 +1,7 @@
-//  ----------------- 26.2 ---------------
-// option 2
-function makeRed() {
-    document.body.style.backgroundColor = 'red';
-}
+console.log('hello from js');
+// console.log(document.body);
 
+//document.body;
 
-// --------------- option 3 ------------------
-//  <button id="make-blue">Make Blue</button> (html file)
-const makeBlueButton = document.getElementById('make-blue');
-
-makeBlueButton.onlick = makeBlue;
-
-function makeBlue() {
-    document.body.style.backgroundColor = 'blue';
-}
-
-//------option - 3.5 ------- directly function boshaye dibo onclick e
-const makePurpleButton = document.getElementById('make-purple');
-
-makePurpleButton.onclick = function makePurple() {
-    document.body.style.backgroundColor = 'purple';
-}
+const tagName = document.getElementsByTagName('h1');
+console.log(tagName);
